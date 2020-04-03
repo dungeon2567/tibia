e724d6bf54b2906e5e6b9352ea6be28b986fa08b
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2020-04-03 13:27:30 -0300

    vahash

--- config.lua
+++ config.lua
@@ -62,7 +62,7 @@ mysqlPass = "otserver"
 mysqlDatabase = "otserver"
 mysqlPort = 3306
 mysqlSock = ""
-passwordType = "sha256"
+passwordType = "vahash";
 
 -- Misc.
 -- NOTE: classicAttackSpeed set to true makes players constantly attack at regular
