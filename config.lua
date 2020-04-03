606a16c348bea448ec1a52bb0a1aaf0d9394328a
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2020-04-03 12:57:06 -0300

    passwordType

--- config.lua
+++ config.lua
@@ -62,6 +62,7 @@ mysqlPass = "otserver"
 mysqlDatabase = "otserver"
 mysqlPort = 3306
 mysqlSock = ""
+passwordType = "sha1"
 
 -- Misc.
 -- NOTE: classicAttackSpeed set to true makes players constantly attack at regular
