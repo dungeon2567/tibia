4e5e7312d46db4d0e04bc11093204edaa251618a
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2020-04-03 21:07:20 -0300

    first items

--- data/creaturescripts/scripts/firstitems.lua
+++ data/creaturescripts/scripts/firstitems.lua
@@ -1,58 +1,62 @@
 local config = {
-    [1] = {
-        -- equipment; Soldier Helmet, Wand of Vortex, Brass Armor, Brass Legs, Amulet of Loss, Black Shield, Leather Boots
-        {
-            {2481, 1}, {2190, 1}, {2465, 1}, {2478, 1}, {2173, 1}, {2529, 1},
-            {2643, 1}
-        },
-        -- container; Rope, Light Shovel, 30 Platinum Coins, 100 Brown Mushroom
-        {{8712, 1}, {5710, 1}, {2152, 30}, {2789, 100}}
-    },
-    [2] = {
-        -- equipment; Soldier Helmet, Snakebite Rod, Brass Armor, Brass Legs, Amulet of Loss, Black Shield, Leather Boots
-        {
-            {2481, 1}, {2182, 1}, {2465, 1}, {2478, 1}, {2173, 1}, {2529, 1},
-            {2643, 1}
-        },
-        -- container; Rope, Light Shovel, 30 Platinum Coins, 100 Brown Mushroom
-        {{8712, 1}, {5710, 1}, {2152, 30}, {2789, 100}}
-    },
-    [3] = {
-        -- equipment; Soldier Helmet, Bow, Brass Armor, Brass Legs, Amulet of Loss, 100 Arrow, Leather Boots
-        {
-            {2481, 1}, {2456, 1}, {2465, 1}, {2478, 1}, {2173, 1}, {2544, 100},
-            {2643, 1}
-        },
-        -- container; Rope, Light Shovel, 30 Platinum Coins, 100 Brown Mushroom
-        {{8712, 1}, {5710, 1}, {2152, 30}, {2789, 100}}
-    },
-    [4] = {
-        -- equipment; Soldier Helmet, Steel Axe, Brass Armor, Brass Legs, Amulet of Loss, Black Shield, Leather Boots
-        {
-            {2481, 1}, {8601, 1}, {2465, 1}, {2478, 1}, {2173, 1}, {2529, 1},
-            {2643, 1}
-        },
-        -- container; Rope, Light Shovel, 30 Platinum Coins, 100 Brown Mushroom, Banana Staff, Jagged Sword
-        {{8712, 1}, {5710, 1}, {2152, 30}, {2789, 100}, {3966, 1}, {8602, 1}}
-    }
+	[0] = {
+		--club, coat
+		items = {{2398, 1}, {2461, 1}, {2467, 1}, {2649, 1}},
+		--container rope, shovel, red apple
+		container = {{2120, 1}, {2554, 1}, {2674, 2}}
+	},
+	[1] = {
+		--equipment spellbook, wand of vortex, magician's robe, mage hat, studded legs, leather boots, scarf
+		items = {{2175, 1}, {2190, 1}, {8819, 1}, {8820, 1}, {2468, 1}, {2643, 1}, {2661, 1}},
+		--container platinum coin, rope, shovel, mana potion
+		container = {{2152, 20}, {7620, 5}, {18559, 1}, {2120, 1}}
+	},
+	[2] = {
+		--equipment spellbook, snakebite rod, magician's robe, mage hat, studded legs, leather boots scarf
+		items = {{2175, 1}, {2182, 1}, {8819, 1}, {8820, 1}, {2468, 1}, {2643, 1}, {2661, 1}},
+		--container platinum coin, rope, shovel, mana potion
+		container = {{2152, 20}, {7620, 5}, {18559, 1}, {2120, 1}}
+	},
+	[3] = {
+		--equipment dwarven shield, 5 spear, ranger's cloak, ranger legs scarf, legion helmet
+		items = {{2525, 1}, {2389, 5}, {2660, 1}, {8923, 1}, {2643, 1}, {2661, 1}, {2480, 1}},
+		--container platinum coin, rope, shovel, health potion, mana potion
+		container = {{2152, 20}, {7618, 5}, {7620, 5}, {18559, 1}, {2120, 1}}
+	},
+	[4] = {
+		--equipment dwarven shield, steel axe, brass armor, brass helmet, brass legs scarf
+		items = {{2525, 1}, {8601, 1}, {2465, 1}, {2460, 1}, {2478, 1}, {2643, 1}, {2661, 1}},
+		--container platinum coin, jagged sword, daramian mace, rope, shovel, health potion, mana potion
+		container = {{2152, 20}, {8602, 1}, {2439, 1}, {7618, 5}, {7620, 5}, {18559, 1}, {2120, 1}}
+	}
+
 }
 
-function onLogin(cid)
-    local player = Player(cid)
-    local targetVocation = config[player:getVocation():getId()]
-    if not targetVocation then return true end
+function onLogin(player)
+	local targetVocation = config[player:getVocation():getId()]
+	if not targetVocation then
+		return true
+	end
 
-    if player:getLastLoginSaved() == 0 then
-        for i = 1, #targetVocation[1] do
-            player:addItem(targetVocation[1][1], targetVocation[1][2])
-		end
-		
-		local backpack = player:addItem(2841)
+	if player:getLastLoginSaved() ~= 0 then
+		return true
+	end
 
-        local backpack = player:addItem(1988)
-        for i = 1, #targetVocation[2] do
-            backpack:addItem(targetVocation[2][1], targetVocation[2][2])
-        end
-    end
-    return true
+	if (player:getSlotItem(CONST_SLOT_LEFT)) then
+		return true
+	end
+
+	for i = 1, #targetVocation.items do
+		player:addItem(targetVocation.items[i][1], targetVocation.items[i][2])
+	end
+
+	local backpack = player:getVocation():getId() == 0 and player:addItem(1987) or player:addItem(1988)
+	if not backpack then
+		return true
+	end
+
+	for i = 1, #targetVocation.container do
+		backpack:addItem(targetVocation.container[i][1], targetVocation.container[i][2])
+	end
+	return true
 end
